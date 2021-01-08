59b1f75b493430373c10b31b5ac960d54dbb768a
Tam Do Minh <daitieuminh@gmail.com>
2021-01-08 23:36:14 +0700

    Added progressBar

--- sources/Assets/Scripts/ProgressBar.cs
+++ sources/Assets/Scripts/ProgressBar.cs
@@ -5,21 +5,37 @@ using UnityEngine.UI;
 
 public class ProgressBar : MonoBehaviour
 {
-    public float Value{
-        get=>slider.value;
-        set=>slider.value = value;
+    public float Value {
+        get => slider.value;
+        set => slider.value = value;
     }
     public Slider slider;
-    public float decreasementSpeed;
+    public float decreasementSpeed = 0.2f;
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+        slider = FindObjectOfType<Slider>();
+    }
+
     void Start()
     {
-        
+        slider.value = 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Decrease the progress bar percent
+        if (slider.value > 0)
+        {
+            slider.value -= decreasementSpeed * Time.deltaTime;
+        }
+    }
+
+    //Increase the progress bar
+    public void Increment(float increaseValue)
+    {
+        slider.value += increaseValue;
     }
 }

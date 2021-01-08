4aee0d5b5b23957f51ef9573b33ac72422d11f79
Viet Nguyen <viet.nguyen.2000@hcmut.edu.vn>
2021-01-08 22:21:29 +0700

    update gamemanger

--- sources/Assets/Scripts/GameManager.cs
+++ sources/Assets/Scripts/GameManager.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public ProgressBar progressBar;
+
     // Start is called before the first frame update
     void Start()
     {
+        progressBar = FindObjectOfType<ProgressBar>();
         
     }
 

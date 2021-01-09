39e2b3b2f1d2adccb8eb037d8f4050b42ebcc303
Viet Nguyen <viet.nguyen.2000@hcmut.edu.vn>
2021-01-10 02:31:50 +0700

    add menu start scene

--- sources/Assets/Scripts/UITextTypeWriter.cs
+++ sources/Assets/Scripts/UITextTypeWriter.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
-
+using UnityEngine.EventSystems;
 // attach to UI Text component (with the full text already there)
 
-public class UITextTypeWriter : MonoBehaviour 
+public class UITextTypeWriter : MonoBehaviour, IPointerClickHandler
 {
 
 	Text txt;
-    public float secsPerText = 0.125f;
+    public float secsPerText = 0.025f;
 	string story;
-
+    bool skip;
 	void Awake () 
 	{
 		txt = GetComponent<Text> ();
@@ -22,13 +22,27 @@ public class UITextTypeWriter : MonoBehaviour
 
 	IEnumerator PlayText()
 	{
+        skip = false;
 		foreach (char c in story) 
 		{
 			txt.text += c;
+            if (skip){
+                txt.text = story;
+                break;
+            }
 			yield return new WaitForSeconds (secsPerText);
             
 		}
-        GetComponentInParent<Animator>().Play("AppearTittle");
+        DoSomethingAfterPlayText();
 	}
+    public virtual void DoSomethingAfterPlayText(){
+
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        skip = true;
+
+    }
 
 }

16004cc5dee83872638c6c10b909c9e7dac41672
TungKhanhTran <tung.tran.1805@hcmut.edu.vn>
2021-01-09 07:08:43 +0700

    temp works

--- sources/Assets/Scripts/Tube.cs
+++ sources/Assets/Scripts/Tube.cs
@@ -1,21 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Tube : MonoBehaviour
-{
+{   
     public Color color{
         get=> spriteColor.color;
         set=> spriteColor.color = value;
     }
     public SpriteRenderer spriteColor;
+    private string spriteColorName = "tube";
+    // TODO
     public Color Merge(Color color1, Color color2){
-
-        return color1 + color2;
+        return (color1 + color2)/2;
     }
     // Start is called before the first frame update
     void Start()
     {
+        spriteColor = Array.Find(GetComponentsInChildren<SpriteRenderer>(), x => x.name == spriteColorName);
         
     }
 
@@ -23,5 +27,12 @@ public class Tube : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // when the GameObjects collider arrange for this GameObject to travel to the left of the screen
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        // spriteMove = -0.1f;
     }
 }

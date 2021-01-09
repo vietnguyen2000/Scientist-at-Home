c3fd64c7ba4cebe7a7848f667f9a6837c8c5e226
Viet Nguyen <viet.nguyen.2000@hcmut.edu.vn>
2021-01-09 10:00:58 +0700

    add game2

---
+++ sources/Assets/Scripts/Game2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game2 : MonoBehaviour
+{
+    public float value = 1;
+    public float BoomRate = 0.2f;
+    private GameManager gameManager;
+    public GameObject Boom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0 ; i < Input.touchCount; i ++){
+            if (Input.touches[i].phase == TouchPhase.Began){
+                gameManager.progressBar.Increment(value);
+                float x = Random.Range(0.0f,1.0f);
+
+            }
+        }
+    }
+}

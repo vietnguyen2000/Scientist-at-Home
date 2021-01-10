49ecf39f72732dead56bce2a88b19b7a2233b67a
Viet Nguyen <viet.nguyen.2000@hcmut.edu.vn>
2021-01-10 15:32:19 +0700

    done retry

---
+++ sources/Assets/Scripts/Retry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Retry : MonoBehaviour
+{
+    public void retry(){
+        Scene scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(scene.name);
+    }
+}
